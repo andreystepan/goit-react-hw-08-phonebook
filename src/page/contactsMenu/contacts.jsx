f33811a5a5afd6a5d7ed30a52b01ba096453391e
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-09-19 23:49:46 +0300

    contacts

--- src/page/contactsMenu/contacts.jsx
+++ src/page/contactsMenu/contacts.jsx
@@ -2,7 +2,6 @@ import { ContactForm } from 'components/ContactForm/contactForm';
 import { ContactList } from 'components/Contacts/ContactList/contactList';
 import { Filter } from 'components/Filter/filter';
 import { Box } from '@mui/material';
-import { Helmet } from 'react-helmet';
 
 export const Contacts = () => {
   return (
@@ -12,9 +11,6 @@ export const Contacts = () => {
         mt: 2,
       }}
     >
-      <Helmet>
-        <title>Contacts</title>
-      </Helmet>
       <ContactForm />
       <Filter />
       <ContactList />
